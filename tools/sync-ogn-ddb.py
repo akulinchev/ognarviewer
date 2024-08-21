f806f4185c390c8c7224bca03bf68c1676912802
Ivan Akulinchev <ivan.akulinchev@gmail.com>
2024-08-21 10:31:33 +0200

    Write the license information to ogn.csv

--- tools/sync-ogn-ddb.py
+++ tools/sync-ogn-ddb.py
@@ -4,11 +4,11 @@ import os
 
 from urllib.request import urlopen
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-with urlopen("https://ddb.glidernet.org/download/") as res:
+with urlopen("https://ddb.glidernet.org/download/") as resp:
     with open(os.path.join(BASE_DIR, "app", "src", "main", "registrations", "ogn.csv"), "w") as f:
-        f.write("#{}\n".format(res.headers.get("Date")))
-        f.write(res.read().decode("utf-8"))
+        f.write("#{}\n".format(resp.headers.get("Date")))
+        f.write("#Exported from ddb.glidernet.org\n")
+        f.write("#License: ODC-BY (https://opendatacommons.org/licenses/by/summary/)\n")
+        f.write(resp.read().decode("utf-8"))
